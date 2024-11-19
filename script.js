601af08a383cfa7d4678502a4c3e49fa7f1a6cd8
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-11-18 21:02:28 -0500

    Implement ball collision.

--- script.js
+++ script.js
@@ -27,14 +27,23 @@ class Body {
         }
     }
 
+    calc_collision_vel_change(other) {
+        const pos_diff = sub(this.pos, other.pos)
+        const pos_diff_norm = norm(pos_diff)
+        const vel_diff = sub(this.vel, other.vel)
+        const mass_factor = 2 * other.mass / (this.mass + other.mass)
+        const vel_change = scale(mass_factor * dot(vel_diff, pos_diff) / (pos_diff_norm * pos_diff_norm), pos_diff)
+        return sub(this.vel, vel_change)
+    }
+
     check_other_collide(other) {
         if(dist(this.pos, other.pos) > this.r + other.r) {
             return;
         }
-        const pos_diff = sub(this.pos, other.pos)
-        console.log(pos_diff)
-        const angle = Math.tan(pos_diff.y, pos_diff.x) * 180 / Math.PI
-        console.log(angle)
+        const vel_change1 = this.calc_collision_vel_change(other)
+        const vel_change2 = other.calc_collision_vel_change(this)
+        this.vel = vel_change1
+        other.vel = vel_change2
     }
 
     draw(ctx) {
@@ -75,6 +84,10 @@ function dist(vec1, vec2) {
     return norm(sub(vec1, vec2))
 }
 
+function dot(vec1, vec2) {
+    return vec1.x * vec2.x + vec1.y * vec2.y
+}
+
 function draw_all(ctx, bodies) {
     ctx.clearRect(0, 0, 500, 500)
     for(let i = 0; i < bodies.length; i++) {
@@ -94,8 +107,10 @@ function main() {
     const ctx = canvas.getContext("2d")
 
     let bodies = []
-    bodies.push(new Body(10, new Vector(300, 250), new Vector(-40, 12)))
-    bodies.push(new Body(10, new Vector(200, 235), new Vector(10, 30)))
+    bodies.push(new Body(10, new Vector(300, 200), new Vector(40, 5)))
+    bodies.push(new Body(10, new Vector(300, 400), new Vector(-30, 45)))
+    bodies.push(new Body(10, new Vector(100, 200), new Vector(-90, 15)))
+    bodies.push(new Body(10, new Vector(200, 210), new Vector(30, 0)))
     bodies.push(new Body(10, new Vector(400, 20), new Vector(85, -70)))
     
     draw_all(ctx, bodies)
